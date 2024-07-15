b0fa0b6ddec1365ef74e88b1c38f76992e9a9091
mhmmadrizaa <mhmmadriza1928@gmail.com>
2024-07-15 11:23:02 +0800

    Menerapkan fungsi validasi ketersediaan meja

--- uaskelompok6.cpp
+++ uaskelompok6.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h> // Tambahkan ini untuk menggunakan tipe bool
 
 // Definisi struct untuk menyimpan data reservasi
 typedef struct {
@@ -8,3 +9,13 @@ typedef struct {
     char tanggal[20];
     char waktu[10];
 } Reservasi;
+
+// Fungsi validasi ketersediaan meja
+bool validasiMeja(Reservasi reservasi[], int jumlahReservasi, const char tanggal[], const char waktu[]) {
+    for(int i = 0; i < jumlahReservasi; i++) {
+        if(strcmp(reservasi[i].tanggal, tanggal) == 0 && strcmp(reservasi[i].waktu, waktu) == 0) {
+            return false; // Meja sudah dipesan pada waktu tersebut
+        }
+    }
+    return true; // Meja tersedia
+}
