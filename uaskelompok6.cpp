edee36c2a7edd8768dab3318c03cb9a12120c6e3
mhmmadrizaa <mhmmadriza1928@gmail.com>
2024-07-15 11:40:04 +0800

    Fungsi validasi ketersediaan meja

--- uaskelompok6.cpp
+++ uaskelompok6.cpp
@@ -68,3 +68,12 @@ void hapusReservasi(Reservasi reservasi[], int *jumlahReservasi) {
         printf("Nomor reservasi tidak valid.\n");
     }
 }
+// Fungsi validasi ketersediaan meja
+int validasiMeja(Reservasi reservasi[], int jumlahReservasi, char tanggal[], char waktu[]) {
+    for(int i = 0; i < jumlahReservasi; i++) {
+        if(strcmp(reservasi[i].tanggal, tanggal) == 0 && strcmp(reservasi[i].waktu, waktu) == 0) {
+            return 0; // Meja sudah dipesan pada waktu tersebut
+        }
+    }
+    return 1; // Meja tersedia
+}
